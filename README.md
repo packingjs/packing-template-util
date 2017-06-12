03e2f7e203629567c4faa0acb078fc75f1c020bf
zhongzhi107 <zhong.zhi@163.com>
2017-06-13 07:12:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# packing-template-util
+
+packing 各类模版中公用方法集
+
+## Commands
+```
+# start
+npm start
+
+# lint
+npm run lint
+
+# build
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { existsSync } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'url';
+import { isFunction } from 'util';
+import assign from 'object-assign-deep';
+import importFresh from 'import-fresh';
+
+const getPath = (req, options) => {
+  const { templates, mockData, extension, globalData } = options;
+  const { pathname } = parse(req.url);
+  const endpoint = options.rewriteRules[pathname] || pathname;
+  const templatePath = resolve(templates, endpoint);
+  const pageDataPath = resolve(mockData, endpoint.replace(extension, '.js'));
+  const globalDataPath = resolve(mockData, globalData);
+  return {
+    templatePath, pageDataPath, globalDataPath
+  };
+};
+const getContext = async (req, res, pageDataPath, globalDataPath) => {
+  let globalContext = {};
+  if (existsSync(globalDataPath)) {
+    const gcontext = importFresh(globalDataPath);
+    if (isFunction(gcontext)) {
+      globalContext = gcontext(req, res);
+      if (globalContext instanceof Promise) {
+        await globalContext.then((data) => {
+          globalContext = data;
+        }, (error) => {
+          console.error(error);
+        });
+      }
+    } else {
+      globalContext = gcontext;
+    }
+  }
+
+  let pageContext = {};
+  if (existsSync(pageDataPath)) {
+    const pcontext = importFresh(pageDataPath);
+    if (isFunction(pcontext)) {
+      pageContext = pcontext(req, res);
+      if (pageContext instanceof Promise) {
+        await pageContext.then((data) => {
+          pageContext = data;
+        }, (error) => {
+          console.error(error);
+        });
+      }
+    } else {
+      pageContext = pcontext;
+    }
+  }
+
+  return assign(globalContext, pageContext);
+};
+
+export default {
+  getPath, getContext
+};
